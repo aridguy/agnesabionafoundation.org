d92d03345ef4b1d0d42f834f25814f3b5d226460
aridguy <rotimiariyo@gmail.com>
2022-06-11 13:22:09 +0100

    gftft

--- src/Components/SideSocialIcons.js
+++ src/Components/SideSocialIcons.js
@@ -5,10 +5,10 @@ const SideSocialIcons = () => {
     return (
         <div>
             <div className="socialbox">
-                <a href="#" id="box-one"><i className="fa fa-facebook"></i></a>
-                <a href="#" id="box-two"><i className="fa fa-twitter"></i></a>
-                <a href="#" id="box-three"><i className="fa fa-google"></i></a>
-                <a href="#" id="box-four"><i className="fa fa-linkedin"></i></a>
+                <a href="agnesabionafoundation.netlify.app" id="box-one"><i className="fa fa-facebook"></i></a>
+                <a href="agnesabionafoundation.netlify.app" id="box-two"><i className="fa fa-twitter"></i></a>
+                <a href="agnesabionafoundation.netlify.app" id="box-three"><i className="fa fa-google"></i></a>
+                <a href="agnesabionafoundation.netlify.app" id="box-four"><i className="fa fa-linkedin"></i></a>
             </div>
         </div>
     );
