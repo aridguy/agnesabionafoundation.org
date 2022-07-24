dd46248a7bfa465d1199f03244b1fc2aac1f8b0f
aridguy <rotimiariyo@gmail.com>
2022-07-24 20:39:01 +0100

    working on the social icons

--- src/Components/SideSocialIcons.js
+++ src/Components/SideSocialIcons.js
@@ -8,7 +8,7 @@ const SideSocialIcons = () => {
                 <a href="agnesabionafoundation.netlify.app" id="box-one"><i className="fa fa-facebook"></i></a>
                 <a href="agnesabionafoundation.netlify.app" id="box-two"><i className="fa fa-twitter"></i></a>
                 <a href="agnesabionafoundation.netlify.app" id="box-three"><i className="fa fa-google"></i></a>
-                <a href="agnesabionafoundation.netlify.app" id="box-four"><i className="fa fa-linkedin"></i></a>
+                <a href="https://www.linkedin.com/in/agnes-abiona-foundation-748472246/" id="box-four"><i className="fa fa-linkedin" target="_blank"></i></a>
             </div>
         </div>
     );
