b0528f286afb2ee35a6f40c82c30b377aa011d93
aridguy <rotimiariyo@gmail.com>
2022-08-02 15:17:55 +0100

    knbk

---
+++ src/Pages/Gallery/Gallery.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Volunteer.css";
+
+const Volunteer = () => {
+    return (
+        <div>
+            <section>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-md-3'></div>
+                        <div className='col-md-6'>
+                            
+                        </div>
+                        <div className='col-md-3'></div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Volunteer;
